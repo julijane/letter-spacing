7640ff4c5a07f372027d9adf50ca23dc2b6a0113
Juliane Holzt <juliane@holzt.de>
2018-11-28 14:50:45 +0100

    testing this would be way to oldschool

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let decapsPromise = async (str, spacing=1) => (await sleep(10)) || decaps(str, s
 // Parse this. This is only for the sake of making it a oneliner, don't write code like this.
 // But congratulations for at least taking a peek into the sourcecode.
 let spacedCake = str=>str.split('').map((v,i)=>spaced(str[i]+(i==0?str.substr(1,str.length-2):i==str.length-1?decaps(str,0).substr(1,str.length-2):' '.repeat(str.length-2))+decaps(str,0)[i]+'\n')).join('')
-let spacedCakePromise = async str => (await sleep(10)) || spaceCube(str)
+let spacedCakePromise = async str => (await sleep(10)) || spacedCube(str)
 
 let spacedCube = str => [...str.split('').map((v,i)=>!i?str:i<str.length-1?str[i]+' '.repeat(str.length-2)+decaps(str,0)[i]+' '.repeat(i-1)+decaps(str,0)[i]:decaps(str,0)+' '.repeat(i-1)+decaps(str,0)[i]),...(str.split('').map((v,i)=>!i?'':i<str.length-1?' '.repeat(i)+decaps(str,0)[i]+' '.repeat(str.length-2)+str[i]+' '.repeat(str.length-i-2)+str[i]:' '.repeat(i)+str)).slice(1)].map(s=>spaced(s)).join('\n')
 let spacedCubePromise = async str => (await sleep(10)) || spacedCake(str)
